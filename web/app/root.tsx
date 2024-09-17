136e0fc467c01c41dfc27aa4a7ef165cfa1289ce
Alexandre <superalexandre0@gmail.com>
2024-09-17 17:39:54 +0200

    feat: ✨ Add milionjs and prod???

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -9,16 +9,16 @@ import {
 
 import stylesheet from "@/tailwind.css?url"
 import Header from "./components/header"
-import { useNetworkConnectivity } from "@remix-pwa/client"
-import { toast } from "sonner"
-import { ManifestLink, useSWEffect } from "@remix-pwa/sw"
+// import { useNetworkConnectivity } from "@remix-pwa/client"
+// import { toast } from "sonner"
+// import { ManifestLink, useSWEffect } from "@remix-pwa/sw"
 
 export const links: LinksFunction = () => [
     { rel: "stylesheet", href: stylesheet },
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
-    useSWEffect()
+    // useSWEffect()
 
     return (
         <html lang="en" className="dark">
@@ -27,7 +27,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
                 <Links />
-                <ManifestLink />
+                {/* <ManifestLink /> */}
 
                 <meta name="theme-color" content="#000000" />
             </head>
@@ -45,25 +45,25 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
 
-    useNetworkConnectivity({
-        onOnline: () => {
-            const id = "network-connectivity"
-            const title = "You are back online"
-            const description = "Seemed your network went for a nap, glad to have you back!"
-            const type = "message"
+    // useNetworkConnectivity({
+    //     onOnline: () => {
+    //         const id = "network-connectivity"
+    //         const title = "You are back online"
+    //         const description = "Seemed your network went for a nap, glad to have you back!"
+    //         const type = "message"
 
-            toast[type](title, { id, description })
-        },
+    //         toast[type](title, { id, description })
+    //     },
 
-        onOffline: () => {
-            const id = "network-connectivity"
-            const title = "You are offline"
-            const description = "Seems like you are offline, check your network connection"
-            const type = "warning"
+    //     onOffline: () => {
+    //         const id = "network-connectivity"
+    //         const title = "You are offline"
+    //         const description = "Seems like you are offline, check your network connection"
+    //         const type = "warning"
 
-            toast[type](title, { id, description })
-        }
-    })
+    //         toast[type](title, { id, description })
+    //     }
+    // })
 
     return <Outlet />
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+import MillionLint from "@million/lint"
 import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
@@ -8,9 +9,28 @@ import { remixPWA } from "@remix-pwa/dev"
 // import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 const isProduction = process.env.NODE_ENV === "production"
-
 console.log("isProduction", isProduction)
-
+const _plugins = [
+    isProduction && react(),
+    remix({
+        routes: defineRoutes => {
+            return flatRoutes("routes", defineRoutes)
+        },
+        //   tailwind: true,
+        //   postcss: true,
+        future: {
+            // eslint-disable-next-line camelcase
+            v3_fetcherPersist: true,
+            // eslint-disable-next-line camelcase
+            v3_relativeSplatPath: true,
+            // eslint-disable-next-line camelcase
+            v3_throwAbortReason: true
+        }
+    }), 
+    remixPWA(), 
+    tsconfigPaths()
+]
+_plugins.unshift(MillionLint.vite())
 export default defineConfig({
     build: {
         // modulePreload: {
@@ -21,36 +41,11 @@ export default defineConfig({
         // }
     },
     server: {
-        port: 4000,
+        port: 4000
     },
     ssr: {
         noExternal: ["react-charts", "remix-utils"]
     },
-    plugins: [
-        isProduction && react(),
-        remix({
-            routes: (defineRoutes) => {
-                return flatRoutes("routes", defineRoutes)
-            },
-            //   tailwind: true,
-            //   postcss: true,
-            future: {
-                // eslint-disable-next-line camelcase
-                v3_fetcherPersist: true,
-                // eslint-disable-next-line camelcase
-                v3_relativeSplatPath: true,
-                // eslint-disable-next-line camelcase
-                v3_throwAbortReason: true,
-            },
-        }),
-        remixPWA({
-            // workerBuildDirectory: "out",
-            // scope: "/pwa",
-        }),
-        tsconfigPaths(),
-        // external({
-        //     includeDependencies: true,
-        // }),
-    ],
+    plugins: _plugins
     // external: ["react", "react-dom"],
 })
