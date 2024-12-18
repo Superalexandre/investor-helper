6e01759c51a399515bdf171e660c8b6e7524c927
Alexandre <superalexandre0@gmail.com>
2024-12-18 13:37:58 +0100

    fix: ⬆️ Fix deps error

--- web/app/entry.server.tsx
+++ web/app/entry.server.tsx
@@ -8,7 +8,7 @@ import { renderToPipeableStream } from "react-dom/server"
 import { createInstance } from "i18next"
 import i18next from "./i18next.server"
 import { I18nextProvider, initReactI18next } from "react-i18next"
-import Backend from "i18next-fs-backend"
+import Backend from "i18next-fs-backend/cjs"
 import i18n from "./i18n"
 import { resolve } from "node:path"
 
