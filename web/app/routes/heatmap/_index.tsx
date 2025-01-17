6e2d77a521f1c2b3729ad3fa024fa664bb1d4cd9
Alexandre <superalexandre0@gmail.com>
2025-01-17 15:33:20 +0100

    feat: ✨ Improve with more sources and better display

--- web/app/routes/heatmap/_index.tsx
+++ web/app/routes/heatmap/_index.tsx
@@ -1,10 +1,13 @@
-import { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-// import { ChartContainer } from "../../components/ui/chart";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { Form, useActionData, useLoaderData, useNavigate, useSearchParams, useSubmit } from "@remix-run/react";
 import { ResponsiveContainer, Tooltip, Treemap } from "recharts";
 import { fetchScreener } from "../../../utils/tradingview/request";
-import { and, ColumnScreenerMappingType, ColumnTypeScreener, ColumnTypeStock, createFilterExpression, createFilterOperation } from "../../../utils/tradingview/filter";
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
+import { and, type ColumnScreenerMappingType, type ColumnTypeScreener, createFilterExpression, createFilterOperation } from "../../../utils/tradingview/filter";
+import { TransformWrapper, TransformComponent, type ReactZoomPanPinchState } from 'react-zoom-pan-pinch'
+import type { ReactNode } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select";
+import logger from "../../../../log";
+import { cn } from "../../lib/utils";
 
 const columns = [
     "close",
@@ -30,9 +33,41 @@ type ResultType = ResultTyped<typeof columns> & {
 type ProcessedDataType = ResultType & {
     size: number
     color: string
+    index: number
+    total: number
 }
 
-export const loader: LoaderFunction = async () => {
+type Market = "SP500" | "CAC40" | "NASDAQ100"
+type Markets = {
+    [key in Market]: {
+        country: string
+        symbolset: string[];
+        markets: string[];
+        range: [number, number]
+    };
+};
+async function getData(market: Market = "SP500") {
+    const markets: Markets = {
+        "SP500": {
+            country: "america",
+            symbolset: ["SYML:SP;SPX"],
+            markets: ["america"],
+            range: [0, 300]
+        },
+        "CAC40": {
+            country: "france",
+            symbolset: ["SYML:EURONEXT;PX1"],
+            markets: ["france"],
+            range: [0, 40]
+        },
+        "NASDAQ100": {
+            country: "america",
+            symbolset: ["SYML:NASDAQ;NDX"],
+            markets: ["america"],
+            range: [0, 300]
+        }
+    }
+
     /*
     [
             {
@@ -52,6 +87,8 @@ export const loader: LoaderFunction = async () => {
         ]
             */
 
+    const selectedMarket = markets[market] || markets.SP500
+
     const filter = and(
         createFilterOperation("market_cap_basic", "nempty"),
         // @ts-ignore
@@ -61,36 +98,78 @@ export const loader: LoaderFunction = async () => {
 
     const { parsedResult } = await fetchScreener({
         labelProduct: "heatmap-stock",
-        country: "america",
+        country: selectedMarket.country,
         columns: columns,
         filter: filter,
         sort: {
             sortBy: "market_cap_basic",
             sortOrder: "desc"
         },
+        // biome-ignore lint/style/useNamingConvention: <explanation>
         ignore_unknown_fields: false,
         options: {
             lang: "fr"
         },
-        range: [0, 300],
-        markets: ["america"],
+        range: selectedMarket.range,
+        markets: selectedMarket.markets,
         symbols: {
-            symbolset: ["SYML:SP;SPX"]
+            symbolset: selectedMarket.symbolset
         }
     })
 
     if (!parsedResult) {
-        return {
-            result: []
-        }
+        logger.error(`No result for ${market} in heatmap`)
+
+        return []
     }
 
+    return parsedResult as ResultType[]
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+
+    const url = new URL(request.url)
+
+    const market = url.searchParams.get("market") as Market | undefined
+
+    const result = await getData(market)
+
     return {
-        result: parsedResult as ResultType[]
+        result
+    }
+
+}
+
+export const action: ActionFunction = async ({ request }) => {
+    const body = await request.formData()
+    let market: Market = "SP500"
+
+    if (body.get("market")) {
+        market = body.get("market") as Market
+    }
+
+    const result = await getData(market)
+
+    return {
+        result
     }
 }
 
-const processData = (data: ResultType[] = []) => {
+// export async function action({ request }: ActionFunctionArgs) {
+//     let market = "SP500"
+//     const body = await request.formData()
+//     if (body.get("market")) {
+//         market = body.get("market") as string
+//     }
+
+//     const data = await getData(market)
+
+//     return {
+//         data
+//     }
+// }
+
+const processData = (data: ResultType[] = []): ProcessedDataType[] => {
     if (!Array.isArray(data) || data.length === 0) {
         return []
     }
@@ -101,45 +180,51 @@ const processData = (data: ResultType[] = []) => {
         ...stock,
         size: totalMarketCap > 0 ? ((stock.market_cap_basic || 0) / totalMarketCap) * 100 : 0,
         color: colors[index % colors.length],
+        index,
+        total: data.length
     }))
 
-    return processed as ProcessedDataType[]
+    return processed
 }
 
-const CustomTooltip = ({ active, payload, transformState }: any) => {
-    if (active && payload && payload.length && payload[0].payload) {
+const CustomTooltip = ({ active, payload }: {
+    active: boolean,
+    payload: {
+        payload: ProcessedDataType
+    }[],
+    transformState: ReactZoomPanPinchState
+}): ReactNode | null => {
+    if (active && payload && payload.length > 0 && payload[0].payload) {
         const data = payload[0].payload as ProcessedDataType
         return (
-            <div className="bg-white p-2 border border-gray-200 rounded shadow" key={data.symbol}>
+            <div className="rounded border border-gray-200 bg-white p-2 shadow" key={data.symbol}>
                 <p className="font-bold text-black">{data.description} ({data.symbol})</p>
                 <p className="text-black">Market Cap: ${((data.market_cap_basic || 0).toLocaleString())} billion</p>
                 <p className="text-black">Weight: {(data.size || 0).toFixed(2)}%</p>
+                <p className="text-black">Position: {data.index + 1}/{data.total}</p>
             </div>
         )
     }
     return null
 }
 
-const CustomizedContent = (props: any) => {
-    if (!props) return null
-
-    const { x, y, width, height, value, description, symbol, change, logoid, transformState } = props as {
-        x: number,
-        y: number,
-        width: number,
-        height: number,
-        value: number,
-        description: ProcessedDataType["description"],
-        symbol: ProcessedDataType["symbol"],
-        change: ProcessedDataType["change"],
-        logoid: ProcessedDataType["logoid"],
-        transformState: {
-            previousScale: number,
-            scale: number,
-            positionX: number,
-            positionY: number,
-        }
+const CustomizedContent = (props: {
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    value: number,
+    description: ProcessedDataType["description"],
+    symbol: ProcessedDataType["symbol"],
+    change: ProcessedDataType["change"],
+    logoid: ProcessedDataType["logoid"],
+    transformState: ReactZoomPanPinchState
+} | null): ReactNode => {
+    if (!props) {
+        return null
     }
+
+    const { x, y, width, height, description, symbol, change, logoid } = props
 
     const colors = {
         darkGreen: {
@@ -166,109 +251,119 @@ const CustomizedContent = (props: any) => {
         }
     }
 
+    const getColor = (change: number): {
+        background: string
+    } => {
+        const thresholds = [
+            { limit: 3, color: colors.darkGreen },
+            { limit: 1.5, color: colors.green },
+            { limit: 0.5, color: colors.lightGreen },
+            { limit: -0.5, color: colors.gray },
+            { limit: -1.5, color: colors.lightRed },
+            { limit: -3, color: colors.red },
+            { limit: Number.NEGATIVE_INFINITY, color: colors.darkRed }
+        ];
 
-    let color = colors.gray
-    if (change >= 3) {
-        color = colors.darkGreen
-    } else if (change >= 1.5) {
-        color = colors.green
-    } else if (change >= 0.5) {
-        color = colors.lightGreen
-    } else if (change <= -3) {
-        color = colors.darkRed
-    } else if (change <= -1.5) {
-        color = colors.red
-    } else if (change < -0.5) {
-        color = colors.lightRed
-    }
+        return thresholds.find(({ limit }) => change >= limit)?.color || colors.gray;
+    };
 
-    const fontSize = 12
+    const color = getColor(change || 0)
 
-    const displayDescription = width > description?.length * fontSize
-    const displaySymbol = width > symbol?.length * fontSize
+    const fontSize = 24
+    const minImageSize = width / 6
 
-    let prettyDescription = description || ""
-
-    if (!displayDescription) {
-        // Truncate description as long as possible
-        prettyDescription = description?.slice(0, Math.floor(width / fontSize)) + "..."
-
-        // If the description is too short, hide it
-        if (prettyDescription.length < 3) {
-            prettyDescription = ""
-        }
-    }
-
-    // const scale = transformState.scale
-
-    // const scaledWidth = width * scale
-    // const scaledHeight = height * scale
-
-    // const scaledX = x * scale
-    // const scaledY = y * scale
-
-    // return (
-    //     <g>
-    //         <rect x={scaledX} y={scaledY} width={scaledWidth} height={scaledHeight} fill={color.background} />
-    //         <text x={scaledX + scaledWidth / 2} y={scaledY + scaledHeight / 2} textAnchor="middle" fill="#fff" fontSize={12} dy={4} className="">
-    //             {prettyDescription}
-    //         </text>
-    //         <text x={scaledX + scaledWidth / 2} y={scaledY + scaledHeight / 2 + 15} textAnchor="middle" fill="#fff" fontSize={fontSize} dy={4}>
-    //             {displaySymbol ? symbol : ""}
-    //         </text>
-    //         <text x={scaledX + scaledWidth / 2} y={scaledY + scaledHeight / 2 + 30} textAnchor="middle" fill="#fff" fontSize={fontSize} dy={4}>
-    //             {`${change?.toFixed(2)}%`}
-    //         </text>
-    //     </g>
-    // )
+    const displayText = height > (fontSize * 3) + minImageSize && width > (description?.length / 2) * fontSize 
+    const bigImage = width < 50 
 
     return (
         <g>
             <rect x={x} y={y} width={width} height={height} fill={color.background} />
+            <foreignObject x={x} y={y} width={width} height={height}>
+                <div className={cn(
+                    "flex size-full flex-col items-center justify-center",
+                    bigImage ? "p-2" : "p-3"
+                )}>
+                    <img
+                        src={`/api/image/symbol?name=${logoid}`}
+                        alt={description}
+                        className={cn(
+                            "mx-auto rounded-full",
+                            displayText ? "w-1/6" : (bigImage ? "w-full" : "w-1/2")
+                        )}
+                    />
 
-            <image 
-                x={x + 5} 
-                y={y + 5} 
-                width={20} 
-                height={20} 
-                href={"/api/image/symbol?name=" + logoid} 
-            />
-
-            <text x={x + width / 2} y={y + height / 2} textAnchor="middle" fill="#fff" fontSize={12} dy={4} className="">
-                {prettyDescription}
-            </text>
-            <text x={x + width / 2} y={y + height / 2 + 15} textAnchor="middle" fill="#fff" fontSize={fontSize} dy={4}>
-                {displaySymbol ? symbol : ""}
-            </text>
-            <text x={x + width / 2} y={y + height / 2 + 30} textAnchor="middle" fill="#fff" fontSize={fontSize} dy={4}>
-                {`${change?.toFixed(2)}%`}
-            </text>
+                    <div
+                        className={cn(
+                            "flex flex-col items-center justify-center w-full",
+                            displayText ? "block" : "hidden"
+                        )}
+                    >
+                        <p className="truncate text-center">{description}</p>
+                        <p className="truncate text-center">{symbol}</p>
+                        <p className="truncate text-center">{change?.toFixed(2)}%</p>
+                    </div>
+                </div>
+            </foreignObject>
         </g>
-    )
+    );
 }
 
-export default function Index() {
+export default function Index(): ReactNode {
     const { result } = useLoaderData<typeof loader>()
+    const resultAction = useActionData<typeof action>()
 
-    if (!result) {
+    const submit = useSubmit()
+    const navigate = useNavigate()
+    const [searchParams] = useSearchParams()
+
+    if (!result || (resultAction && !resultAction.result)) {
         return <p>No Data</p>
     }
 
-    const processedData = processData(result as ResultType[])
+    const processedData = processData(resultAction?.result ?? result as ResultType[])
 
     if (!processedData || processedData.length === 0) {
-        return <p>No Data</p>
+        return <p>No Data (processed)</p>
     }
 
+
+    const handleSubmit = (value: string): void => {
+        const formData = new FormData()
+
+        formData.append("market", value)
+
+        submit(formData, { method: "post" })
+
+        navigate(`/heatmap?market=${value}`)
+    }
+
+    const market = searchParams.get("market") || "SP500"
+
     return (
-        <div className="w-full h-[calc(100vh-64px)] overflow-hidden relative">
-            <div className="w-full h-full">
+        <div className="relative h-[calc(100vh-64px)] w-full overflow-hidden">
+
+            <Form className="absolute top-0 left-0" method="POST">
+                <div className="absolute top-0 left-0 z-10 m-4">
+                    <Select name="market" defaultValue={market} onValueChange={(value) => handleSubmit(value)}>
+                        <SelectTrigger className="bg-background">
+                            <SelectValue placeholder="Choisir un marché" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="SP500">S&P 500</SelectItem>
+                            <SelectItem value="CAC40">CAC 40</SelectItem>
+                            <SelectItem value="NASDAQ100">NASDAQ 100</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </Form>
+
+            <div className="h-full w-full">
                 <TransformWrapper
                     initialScale={1}
                     initialPositionX={0}
                     initialPositionY={0}
                 >
-                    {({ zoomIn, zoomOut, resetTransform, instance }) => (
+                    {({ instance }): ReactNode => (
                         <div className="h-full w-full">
                             {/* <div className="absolute top-4 left-4 z-10 space-x-2">
                                 <button onClick={() => zoomIn()} className="bg-blue-500 text-white px-4 py-2 rounded">Zoom In</button>
@@ -277,15 +372,17 @@ export default function Index() {
                                 <button onClick={() => console.log(instance.transformState)} className="bg-blue-500 text-white px-4 py-2 rounded">Instance</button>
                             </div> */}
                             <TransformComponent contentClass="!w-full !h-full" wrapperClass="!w-full !h-full">
-                                <div className="w-full h-full">
+                                <div className="h-full w-full">
                                     <ResponsiveContainer width="100%" height="100%">
                                         <Treemap
                                             isAnimationActive={false}
                                             data={processedData}
                                             dataKey="size"
                                             aspectRatio={4 / 3}
+                                            // @ts-expect-error - Props are automatically passed to the content component
                                             content={<CustomizedContent transformState={instance.transformState} />}
                                         >
+                                            {/* @ts-expect-error - Props are automatically passed to the content component */}
                                             <Tooltip content={<CustomTooltip transformState={instance.transformState} />} />
                                         </Treemap>
                                     </ResponsiveContainer>
